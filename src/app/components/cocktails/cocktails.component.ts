5cebeeb06534c168c7396395e6151c84d3388ff2
quentin <quentin@dyma.fr>
2024-12-06 16:47:50 +0100

    feat: code partie 2

---
+++ src/app/components/cocktails/cocktails.component.ts
@@ -0,0 +1,46 @@
+import { Component, computed, effect, signal } from '@angular/core';
+import { CocktailsListComponent } from './components/cocktails-list.component';
+import { CocktailDetailsComponent } from './components/cocktail-details.component';
+import { Cocktail } from 'app/shared/interfaces';
+import { cocktails } from 'app/shared/data';
+
+@Component({
+  selector: 'app-cocktails',
+  imports: [CocktailsListComponent, CocktailDetailsComponent],
+  template: `
+    <app-cocktails-list
+      (selectCocktail)="selectCocktail($event)"
+      [selectedCocktailName]="selectedCocktailName()"
+      [cocktails]="cocktails()"
+      class="w-half xs-w-full card"
+    />
+    <app-cocktail-details
+      [cocktail]="selectedCocktail()"
+      class="w-half xs-w-full card"
+    />
+  `,
+  styles: `
+    :host {
+      display: flex;
+      gap:24px;
+      padding: 24px;
+      @media screen and (max-width: 820px) {
+        flex-direction: column;
+      }
+    }
+  `,
+})
+export class CocktailsComponent {
+  cocktails = signal<Cocktail[]>(cocktails);
+  selectedCocktail = signal<Cocktail>(this.cocktails()[0]);
+  selectedCocktailName = computed(() => this.selectedCocktail().name);
+
+  selectCocktail(cocktailName: string) {
+    const newCocktail = this.cocktails().find(
+      ({ name }) => name === cocktailName
+    );
+    if (newCocktail) {
+      this.selectedCocktail.set(newCocktail);
+    }
+  }
+}
---
+++ src/app/components/cocktails/components/cocktails-list.component.ts
@@ -0,0 +1,43 @@
+import { Component, computed, input, output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Cocktail } from 'app/shared/interfaces';
+
+@Component({
+  selector: 'app-cocktails-list',
+  imports: [FormsModule],
+  template: `
+    <h2 class="mb-20">Liste des cocktails</h2>
+    <input
+      [(ngModel)]="filter"
+      type="text"
+      class="mb-20 w-full"
+      placeholder="Chercher un cocktail"
+    />
+    <ul class="mb-20">
+      @for (cocktail of filteredCocktails(); track cocktail.name) { @let active
+      = cocktail.name === selectedCocktailName();
+      <li
+        [class.active-item]="active"
+        [class.text-primary]="active"
+        (click)="selectCocktail.emit(cocktail.name)"
+        class="px-12 py-6 my-2 radius"
+      >
+        <h3>{{ cocktail.name }}</h3>
+      </li>
+      }
+    </ul>
+    <button class="btn btn-primary">Ajouter un cocktail</button>
+  `,
+  styles: `li:hover { cursor:pointer; background-color: var(--light); transition: all 0.4s } `,
+})
+export class CocktailsListComponent {
+  filter = signal('');
+  cocktails = input<Cocktail[]>();
+  filteredCocktails = computed(() =>
+    this.cocktails()?.filter(({ name }) =>
+      name.toLowerCase().includes(this.filter().toLowerCase())
+    )
+  );
+  selectedCocktailName = input.required();
+  selectCocktail = output<string>();
+}
